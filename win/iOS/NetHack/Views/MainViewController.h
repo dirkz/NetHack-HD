6be1636e21af4dc75bd251b500b15a4dcf1feb34
Dirk Zimmermann <me+github@dirkz.com>
2012-02-01 17:35:20 +0100

    __MyCompanyName__

--- win/iOS/NetHack/Views/MainViewController.h
+++ win/iOS/NetHack/Views/MainViewController.h
@@ -3,7 +3,7 @@
 //  NetHack
 //
 //  Created by Dirk Zimmermann on 2/1/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Dirk Zimmermann. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
