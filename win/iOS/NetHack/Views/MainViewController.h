7ae96d53d9e26bdd2f0e716c58e8abe64ecb8c84
Dirk Zimmermann <me+github@dirkz.com>
2012-02-01 17:33:59 +0100

    Added MainStoryboard

---
+++ win/iOS/NetHack/Views/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  NetHack
+//
+//  Created by Dirk Zimmermann on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
