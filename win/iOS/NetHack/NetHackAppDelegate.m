88f1e9d2780831003ac6e31b0733cc401a55b69a
Dirk Zimmermann <me+github@dirkz.com>
2012-02-01 17:48:25 +0100

    Fixed: Applications are expected to have a root view controller at the end of application launch (removed window code from didFinishLaunchingWithOptions)

--- win/iOS/NetHack/NetHackAppDelegate.m
+++ win/iOS/NetHack/NetHackAppDelegate.m
@@ -20,10 +20,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
