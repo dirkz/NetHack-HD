a56ac2deca2edbeccc5f8664604fe7299c6b0844
Dirk Zimmermann <me+github@dirkz.com>
2012-02-01 17:04:05 +0100

    New xcode 4.2.1 project, empty window

---
+++ win/iOS/NetHack/NetHackAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NetHackAppDelegate.h
+//  NetHack
+//
+//  Created by Dirk Zimmermann on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NetHackAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ win/iOS/NetHack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetHack
+//
+//  Created by Dirk Zimmermann on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NetHackAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NetHackAppDelegate class]));
+    }
+}
